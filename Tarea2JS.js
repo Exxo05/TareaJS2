d162bfa7516d36ec863ed65e4a41a3c0b3ce4ecd
Exxo05 <mrosarioperezcejuela@gmail.com>
2025-01-14 18:33:56 +0100

    Se añadió el contenido del fichero

---
+++ Tarea2JS.js
@@ -0,0 +1,10 @@
+const usuario = "Mario";
+const dni = "09072419T";
+let color_fav = "negro";
+let num_tel = 681330526;
+let direccion = "Calle Almanaque";
+console.log("Te llamas " +usuario, ",tu DNI es " +dni, ",tu color favorio es " +color_fav, ",tu numero de telefono es " +num_tel, ",y para acabar tu direcion es " +direccion);
+
+color_fav = "azul";
+direccion = "Calle Alcalá";
+console.log(" Perdon, tu color favorito es " +color_fav, "y tu direcion es" +direccion);
